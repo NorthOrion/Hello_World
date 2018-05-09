1808ed6f52bf3774cd6ea744f57d529e9c5cba43
NorthOrion <39065667+NorthOrion@users.noreply.github.com>
2018-05-09 16:30:11 +0200

    Solved extra 2

---
+++ Python Learning/02 Odd or Even/exercise_2_extra_2.py
@@ -0,0 +1,18 @@
+# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+# Extras:
+
+# If the number is a multiple of 4, print out a different message.
+# Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+try:
+	print("Hello, please submit an integer to check :")
+	num = int(input())
+	print("please submit an integer to divide by")
+	check = int(input())
+	if num % check:
+		print("%d does not divide odd %d" % (check, num))
+	else:
+		print("%d divides odd %d" % (check, num))
+except ValueError:
+	print("Invalid input : Program will terminate")
